bd9226a174aefe5614888dd00dcce18b8e8d68ba
iamilyasedunov <36935089+iamilyasedunov@users.noreply.github.com>
2019-03-24 16:35:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,37 +9,8 @@
 #include "EmployeeFactory.h"
 
 int main(){
-<<<<<<< HEAD
     EmployeeFactory EF;
     EF.creator();
-=======
-    //std::cout<<D.get_income(8)<<" "<<C.get_income(12)<<std::endl;
-
-    //Engineer E(13, "Ee", 12);
-
-    /*
-    Driver C(0, "", 12, 12);
-    Cleaner D(0, "", 12, 12);
-    Programmer P(0, "", 12, 12);
-    Tester T(0, "", 12, 12);
-    TeamLeader TL(0, "", 12, 12);
-    ProjectManager PM(0, "", P1);
-    SeniorManager SM(0, "", P1, 0);
-    D.create();
-    C.create();
-    P.create();
-    T.create();
-    TL.create();
-    PM.create();
-    SM.create();
-    */
-    //CleanerCreator CC;
-    //DriverCreator DC;
-    ProgrammerCreator PC;
-    TesterCreator TC;
-    TLCreator TLC;
-    PMCreator PMC;
-    SMCreator SMC;
->>>>>>> 07f3f043e50f5360d7cc0493574e9dd61894e911
     return 0;
 }
+
